ea650d14d84f2267a7dd8657ad022572a5c14786
Firas Ghanmi <fghanmi@redhat.com>
2024-08-17 22:27:11 +0200

    review-updates
    Signed-off-by: Firas Ghanmi <fghanmi@redhat.com>

--- tuftool/src/add_role.rs
+++ tuftool/src/add_role.rs
@@ -130,7 +130,7 @@ impl AddRoleArgs {
             None => &vec![],
         };
         if passwords.len() > self.keys.len() {
-            panic!("More passwords provided than key sources");
+            error::MorePasswordsSnafu.fail()?;
         }
         for (i, source) in self.keys.iter().enumerate() {
             let password = passwords.get(i).unwrap_or(&default_password);
@@ -173,7 +173,7 @@ impl AddRoleArgs {
             None => &vec![],
         };
         if passwords.len() > self.keys.len() {
-            panic!("More passwords provided than key sources");
+            error::MorePasswordsSnafu.fail()?;
         }
         for (i, source) in self.keys.iter().enumerate() {
             let password = passwords.get(i).unwrap_or(&default_password);
