602f51825850e4a71dcc2e7358d5ea1edb4301f2
SequeI <asiek@redhat.com>
2024-09-23 13:55:00 +0100

    More tests

--- tuftool/tests/update_rhtas_command.rs
+++ tuftool/tests/update_rhtas_command.rs
@@ -334,3 +334,73 @@ async fn update_command_without_metadata_flags() {
     assert_eq!(repo.timestamp().signed.version.get(), 1);
 }
 
+#[tokio::test]
+// Makes use of the --force-version flag to forcefully change version of timestamp metadata while
+// preserving all other
+async fn update_command_force_flag() {
+    let root_json = test_utils::test_data().join("simple-rsa").join("root.json");
+    let root_json2 = test_utils::test_data().join("simple-rsa").join("root.json");
+    let root_key = test_utils::test_data().join("snakeoil.pem");
+    let repo_dir = TempDir::new().unwrap();
+
+    // Create a repo using tuftool and the reference tuf implementation data
+    create_repo(repo_dir.path());
+
+    // Load the created repo
+    let create_repo = RepositoryLoader::new(
+        &tokio::fs::read(root_json2).await.unwrap(),
+        dir_url(&repo_dir),
+        dir_url(repo_dir.path().join("targets")),
+    )
+    .load()
+    .await
+    .unwrap();
+
+    let metadata_base_url = &dir_url(&repo_dir);
+    let update_out = TempDir::new().unwrap();
+
+    // Update the repo we just created
+    Command::cargo_bin("tuftool")
+        .unwrap()
+        .args([
+            "update",
+            "-o",
+            update_out.path().to_str().unwrap(),
+            "-k",
+            root_key.to_str().unwrap(),
+            "--root",
+            root_json.to_str().unwrap(),
+            "--metadata-url",
+            metadata_base_url.as_str(),
+            "--force-version",
+            "--timestamp-version",
+            "5"
+        ])
+        .assert()
+        .success();
+
+    // Load the updated repo
+    let repo = RepositoryLoader::new(
+        &tokio::fs::read(root_json).await.unwrap(),
+        dir_url(&update_out),
+        dir_url(update_out.path().join("targets")),
+    )
+    .load()
+    .await
+    .unwrap();
+
+    // Ensure all the existing targets are accounted for
+    assert_eq!(repo.targets().signed.targets.len(), 3);
+
+    // Ensure all the metadata has been updated
+    assert_eq!(repo.targets().signed.expires, create_repo.targets().signed.expires);
+    assert_eq!(repo.targets().signatures, create_repo.targets().signatures);
+    assert_eq!(repo.targets().signed.version.get(), 1);
+    assert_eq!(repo.snapshot().signed.expires, create_repo.snapshot().signed.expires);
+    assert_eq!(repo.snapshot().signatures, create_repo.snapshot().signatures);
+    assert_eq!(repo.snapshot().signed.version.get(), 1);
+    assert_eq!(repo.timestamp().signed.expires, create_repo.timestamp().signed.expires);
+    assert_ne!(repo.timestamp().signatures, create_repo.timestamp().signatures);
+    assert_eq!(repo.timestamp().signed.version.get(), 5);
+}
+
