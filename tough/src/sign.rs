4223fb9f4267c1df29c346666c2833c42f1e9ddd
Firas Ghanmi <fghanmi@redhat.com>
2025-06-16 17:18:08 +0200

    update

--- tough/src/sign.rs
+++ tough/src/sign.rs
@@ -189,10 +189,10 @@ pub fn decrypt_key(
 /// Accepted Keys: ED25519 pkcs8, Ecdsa pkcs8, RSA
 pub fn parse_keypair(key: &[u8], password: Option<&str>) -> Result<impl Sign> {
     let decrypted_key = if let Some(pw) = password {
-        if !pw.is_empty() {
-            decrypt_key(key, pw).map_err(|_| error::KeyDecryptionSnafu.build())?
-        } else {
+        if pw.is_empty() {
             key.to_vec()
+        } else {
+            decrypt_key(key, pw).map_err(|_| error::KeyDecryptionSnafu.build())?
         }
     } else {
         key.to_vec()
